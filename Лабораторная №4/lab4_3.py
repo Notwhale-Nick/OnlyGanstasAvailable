cad2191d5a3f68c22e4e5c29fe2d7f61bca99537
Notwhale-Nick <r.vaar@yandex.ru>
2026-06-12 17:02:48 +0300

    Update lab4_3.py

--- Лабораторная №4/lab4_3.py
+++ Лабораторная №4/lab4_3.py
@@ -1,15 +1,15 @@
 tup = tuple(map(int, input("Введите элементы кортежа через пробел: ").split()))
 
-uporyadochen = True
+org = True
 first_bad_index = -1
 
 for i in range(len(tup) - 1):
     if tup[i] > tup[i + 1]:  
-        uporyadochen = False
+        org = False
         first_bad_index = i + 1  
         break
 
-if uporyadochen:
+if org:
     print("Кортеж упорядочен по возрастанию")
 else:
     print("Кортеж НЕ упорядочен")
