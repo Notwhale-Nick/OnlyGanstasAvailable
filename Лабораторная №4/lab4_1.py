cbbc13895d1aee27b6370f514ba4f516e7a37a8d
Notwhale-Nick <r.vaar@yandex.ru>
2026-06-12 16:19:08 +0300

    Update lab4_1.py

--- Лабораторная №4/lab4_1.py
+++ Лабораторная №4/lab4_1.py
@@ -1,1 +1,18 @@
+A = [0] * 10
 
+print("Введите 10 чисел:")
+for i in range(10):
+    A[i] = int(input())
+
+result = 1
+pos = False
+
+for i in range(10):
+    if A[i] > 0:
+        result = result * A[i]
+        pos = True
+
+if pos:
+    print(result)
+else:
+    print(0)
