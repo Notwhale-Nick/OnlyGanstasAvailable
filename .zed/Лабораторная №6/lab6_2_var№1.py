99401ba4d30dc451ef17bd5f4eb8efa5e65bdccf
Notwhale-Nick <r.vaar@yandex.ru>
2026-06-12 18:11:53 +0300

    Update lab6_2_var№1.py

--- .zed/Лабораторная №6/lab6_2_var№1.py
+++ .zed/Лабораторная №6/lab6_2_var№1.py
@@ -1,1 +1,33 @@
+def calc_all(*args, operation="sum"):
+    """
+    Вычисляет сумму или произведение всех переданных чисел.
+    
+    Параметры:
+    *args - произвольное количество чисел
+    operation - "sum" для суммы, "mul" для произведения
+    
+    Возвращает:
+    Результат вычисления или None, если чисел нет
+    """
+    if not args: 
+        return None
+    
+    if operation == "sum":
+        result = 0
+        for num in args:
+            result += num
+        return result
+    elif operation == "mul":
+        result = 1
+        for num in args:
+            result *= num
+        return result
+    else:
+        return None  
 
+if __name__ == "__main__":
+    print(calc_all(1, 2, 3, operation="sum"))  
+    print(calc_all(2, 3, 4, operation="mul"))  
+    print(calc_all(5, operation="sum"))        
+    print(calc_all())                          
+    print(calc_all(10, 20, operation="sum"))  
